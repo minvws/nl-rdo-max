4f593198693fd499fffd49f55f5a6c5c42320701
maxxiefjv <vasterd97@gmail.com>
2021-05-07 15:06:24 +0200

    added saml initial settings and django setup

--- inge6/settings.py
+++ inge6/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +27,7 @@ SECRET_KEY = 'django-insecure-b$kz$@4gpqb)or_$79fkej26a8z++x$&#9wz9r6hunmbsxpccx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["10.48.118.250"]
 
 # Application definition
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'inge6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SAML_FOLDER = os.path.join(BASE_DIR, 'saml')
