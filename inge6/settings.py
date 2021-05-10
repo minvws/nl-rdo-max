5e90c4c2d3020ed563bcc528f29b0c9d7b82692f
Max Vasterd <vasterd97@gmail.com>
2021-05-10 20:04:17 +0200

    added rijksoverheid rijksstijl

--- inge6/settings.py
+++ inge6/settings.py
@@ -120,6 +120,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.getenv('STATIC_FILES')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
