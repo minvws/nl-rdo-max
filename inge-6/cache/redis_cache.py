6a61f6fc64616c061b2411854a5c9e1dec968807
Max Vasterd <vasterd97@gmail.com>
2021-05-20 11:50:24 +0200

    prefix redis interaction

--- inge-6/cache/redis_cache.py
+++ inge-6/cache/redis_cache.py
@@ -1,22 +1,23 @@
 from typing import Any
 import pickle
 
-import redis
+from redis import Redis
 
 from ..config import settings
 
 class RedisCache:
+    KEY_PREFIX = 'TVS-'
     EXPIRES_IN_S = 60 * 15
 
     def __init__(self):
-        self.redis_client = redis.Redis(host=settings.redis.host, port=settings.redis.port, db=0)
+        self.redis_client = Redis(host=settings.redis.host, port=settings.redis.port, db=0)
 
     def set(self, name: str, value: Any):
         serialized_value = pickle.dumps(value)
-        self.redis_client.set(name, serialized_value, ex=self.EXPIRES_IN_S)
+        self.redis_client.set(self.KEY_PREFIX + name, serialized_value, ex=self.EXPIRES_IN_S)
 
     def get(self, name):
-        serialized_value = self.redis_client.get(name)
+        serialized_value = self.redis_client.get(self.KEY_PREFIX + name)
         return pickle.loads(serialized_value) if serialized_value else None
 
     def gen_token(self):
