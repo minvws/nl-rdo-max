33b2c14f42e319ec9ba44c1e282051e24727566a
maxxiefjv <vasterd97@gmail.com>
2021-05-26 01:30:50 +0200

    update run server command and clients json

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 [[ -d .venv ]] && source .venv/bin/activate
 
-uvicorn inge-6.main:app --reload --host 127.0.0.1 --port 8006
+#uvicorn inge-6.main:app --reload --host 0.0.0.0 --port 8006
+python3 -m inge-6.main
