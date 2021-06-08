83dd0eab3beb7e82bb91d446a5a24a2bb99e4955
Willem van Asperen <Willem.van.Asperen@paconsulting.com>
2021-06-08 20:47:26 +0200

    first implementation

--- tools/get_tokens.py
+++ tools/get_tokens.py
@@ -1,6 +1,33 @@
+from typing import Union
 import sys
+import requests
+import argparse
+import json
+import logging
 
-import
+
+def retrieve_hashes(endpoint: str, token: str) -> Union[None, dict]:
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "CoronaCheck-Protocol-Version": "3.0",
+    }
+    result = requests.post(endpoint, headers=headers)
+    if result.status_code == 200:
+        return result.json()
+    logging.error(f"{result.status_code} failed request for token {token}")
+    return None
+
 
 if __name__ == "__main__":
-    for jwt_line in sys.stdin.readline():
+
+    parser = argparse.ArgumentParser(description="use id_token to retrieve identity hashes")
+    parser.add_argument('--url',
+                        default='https://holder-api.acc.coronacheck.nl/v4/holder/access_tokens',
+                        type=str,
+                        help='endpoint to receive identity hashes from'
+    )
+    config = parser.parse_args()
+
+    for jwt_line in sys.stdin:
+        jwt_token = jwt_line[:-1]
+        print(retrieve_hashes(config.url, jwt_token))
