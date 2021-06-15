e27dabd33486523ece0c6e9e235c30a2b94164ac
maxxiefjv <vasterd97@gmail.com>
2021-06-15 14:00:40 +0200

    added Redis SSL support

--- inge6/cache/__init__.py
+++ inge6/cache/__init__.py
@@ -9,6 +9,16 @@ _REDIS_CLIENT: Optional[StrictRedis] = None
 def get_redis_client() -> StrictRedis:
     global _REDIS_CLIENT
     if _REDIS_CLIENT is None:
-        _REDIS_CLIENT = StrictRedis(host=settings.redis.host, port=settings.redis.port, db=0)
+        ssl = (True if settings.redis.ssl.lower() == 'true' else False)
+
+        if ssl:
+            _REDIS_CLIENT = StrictRedis(
+                                host=settings.redis.host, port=settings.redis.port, db=0,
+                                ssl=True, 
+                                ssl_keyfile=settings.redis.key, ssl_certfile=settings.redis.cert,
+                                ssl_ca_certs=settings.redis.cafile
+                            )
+        else:
+            _REDIS_CLIENT = StrictRedis(host=settings.redis.host, port=settings.redis.port, db=0)
 
     return _REDIS_CLIENT
