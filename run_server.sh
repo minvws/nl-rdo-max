2e0478595fd3fa6558121e17e2ecf4c833375952
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-06-23 17:18:39 +0200

    Add copyright clauses where missing

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#
+# Copyright (c) 2020-2021 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+#
+# Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+#
+# SPDX-License-Identifier: EUPL-1.2
+#
+
 [[ -d .venv ]] && source .venv/bin/activate
 
 #uvicorn inge-6.main:app --reload --host 0.0.0.0 --port 8006
