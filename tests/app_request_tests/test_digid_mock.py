5a428cda2e3544977341beec5255f465abb0731d
Max Vasterd <vasterd97@gmail.com>
2021-08-13 15:03:48 +0200

    add vulnerability test 2

--- tests/app_request_tests/test_digid_mock.py
+++ tests/app_request_tests/test_digid_mock.py
@@ -2,7 +2,10 @@ from fastapi.testclient import TestClient
 
 from inge6.main import app
 
-
+def test_login_digid():
+    client = TestClient(app)
+    response = client.get("https://tvs.acc.coronacheck.nl/login-digid?force_digid=1&state=%22%3E%3Cscript%3Ealert(%27XSS%27)%3C%2fscript%3E")
+    assert "<script>" not in response.text
 
 def test_digid_mock():
     client = TestClient(app)
