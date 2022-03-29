afda6846f51ad6a6e66c8d111850a9a3028e01c6
Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>
2022-03-29 12:18:35 +0200

    Import config file in settings module
    Signed-off-by: Cochise Ruhulessin <cochise.ruhulessin@unimatrixone.io>

--- inge6/runtime/settings/__init__.py
+++ inge6/runtime/settings/__init__.py
@@ -1,6 +1,10 @@
 """Settings module for :mod:`inge6`."""
 import os
 
+from inge6.config import get_settings
+
+
+cfg = get_settings()
 
 DEPLOYMENT_ENV = os.getenv('DEPLOYMENT_ENV') or 'production'
 
