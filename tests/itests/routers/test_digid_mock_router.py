16eecf3906d6fec55e7093160343a0c20b4da51c
Gerbrand Bosch <gerbrandb@gmail.com>
2022-10-27 09:06:15 +0200

    Upgrade to python 3.10

--- tests/itests/routers/test_digid_mock_router.py
+++ tests/itests/routers/test_digid_mock_router.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-
-digid_mock_app = FastAPI()
-digid_mock_app.routes.append(digid_router)
-
-@pytest.fixture
-def digid_mocked_router()
-    TestClient(digid_mock_app)
+# import pytest
+# from fastapi import FastAPI
+# from fastapi.testclient import TestClient
+#
+# from app.routers.digid_mock_router import digid_mock_router
+#
+# digid_mock_app = FastAPI()
+# digid_mock_app.include_router(digid_mock_router)
+# digid_mock_app.
+#
+# @pytest.fixture
+# def digid_mocked_router():
+#     TestClient(digid_mock_app)
