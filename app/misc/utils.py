1ddb3ef1df593c9ac9116eb716cfb0763d6fb2ec
Ruben van der Knaap <rubenvanderknaap@live.nl>
2023-07-28 12:09:24 +0200

    comment & linting

--- app/misc/utils.py
+++ app/misc/utils.py
@@ -91,10 +91,16 @@ def kid_from_certificate(certificate: str) -> str:
 
 
 def mocked_bsn_to_uzi_data(
-    bsn: str, id_filter: str = None, filepath: str = config.get("app", "mocked_uzi_data_file_path"),
+    bsn: str,
+    relation_id_filter: str = None,
+    filepath: str = config.get("app", "mocked_uzi_data_file_path"),
 ) -> UziAttributes:
     uzi_data = json_from_file(filepath)
     instance = UziAttributes(**uzi_data[bsn])
-    if id_filter:
-        instance.relations = [relation for relation in instance.relations if relation.ura == id_filter]
+    if relation_id_filter:
+        instance.relations = [
+            relation
+            for relation in instance.relations
+            if relation.ura == relation_id_filter
+        ]
     return instance
