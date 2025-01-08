9d7840a3891be3a989602f22e2dcc93c7a2104c9
Jeroen van Schie <jeroen_van_schie@hotmail.com>
2025-01-08 11:13:34 +0100

    build: create node_modules and static assets in Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-if [ "$INSTALL_NPM_ASSETS" == "true" ]; then
-    echo "Setup NPM..."
-    ./scripts/setup-npm.sh
-
-    echo "NPM build..."
-    npm run build
-fi
-
 . .venv/bin/activate
 
 exec "$@"
