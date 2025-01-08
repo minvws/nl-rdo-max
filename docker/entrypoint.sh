c71e3a50b6ae565c8b0a1cc7a6c97cc022abbfcf
Jeroen van Schie <jeroen_van_schie@hotmail.com>
2025-01-08 09:32:27 +0100

    build: create venv in docker file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,11 +5,7 @@ set -e
 if [ "$INSTALL_NPM_ASSETS" == "true" ]; then
     echo "Setup NPM..."
     ./scripts/setup-npm.sh
-fi
 
-make venv
-
-if [ "$INSTALL_NPM_ASSETS" == "true" ]; then
     echo "NPM build..."
     npm run build
 fi
