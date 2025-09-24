24f307d61c9af89de93cd3640098f1fb25fe0f10
Rick Lambrechts <rick@rl-webdiensten.nl>
2025-09-24 19:47:25 +0200

    Set typing in CertificateWithJWK

--- app/models/certificate_with_jwk.py
+++ app/models/certificate_with_jwk.py
@@ -1,5 +1,11 @@
+from cryptography.x509 import Certificate
+from jwcrypto.jwk import JWK
+
+
 class CertificateWithJWK:
-    def __init__(self, certificate, jwk, kid, x5t, pem):
+    def __init__(
+        self, certificate: Certificate, jwk: JWK, kid: str, x5t: str, pem: str
+    ):
         self.certificate = certificate
         self.jwk = jwk
         self.kid = kid
